63976d299454a27aa0a49efd20c9580ce45bd2fe
ReddishYeti7847 <noriyasu200081@gmail.com>
2019-02-02 17:56:35 +0900

    基本的な変数の定義

--- app/src/main/java/com/example/user/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/user/calculatorapp/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.user.calculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    //変数の定義
+    double num[] = new double[3];   //入力した数字
+    int mode;   //今の四則演算記号
+    int state;  //入れてる変数の番号や答え
+
+    //部品の定義
+    TextView text;
+    Button button_1;
+    Button button_plus;
+    Button button_enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        button_1 = findViewById(R.id.button_1);
+        button_plus = findViewById(R.id.button_puls);
+        button_enter = findViewById(R.id.button_enter);
+
+
     }
 }
